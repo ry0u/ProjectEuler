088f5890f81ba2813d191e8d6896b52a4ade2bd3
ry0u <ry0u_yd@yahoo.co.jp>
2015-11-24 11:37:09 +0900

    factor factorization

--- 003.hs
+++ 003.hs
@@ -2,10 +2,17 @@ import Control.Monad
 import Control.Applicative
 import Data.List
 
+-- 約数
+factors :: Integer -> [Integer]
+factors n = [x | x <- [1 .. n], n `mod` x == 0]
+
 -- 素因数分解
-func :: Integer -> [Integer]
-func n = 
-  [1 .. n]
+factorization :: Integer -> [Integer]
+factorization 1 = []
+factorization x = v : factorization(x `div` v)
+  where
+    v = (factors x) !! 1
+
 main = do
-  let a = 10
-  putStrLn $ show $ func a
+  let a = 600851475143
+  print $ maximum $ factorization a
